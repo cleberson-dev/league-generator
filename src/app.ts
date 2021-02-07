4d48d6c76e064932292e243ad3ecd88d3c2c7290
Cleberson F R Junior <cleberson.dev@gmail.com>
2021-02-07 01:26:12 -0300

    Set up morgan

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,28 @@
 import express, { Request, Response } from "express";
+import morgan from 'morgan';
+import fs from 'fs';
 import graphQLAPI from './graphql';
+import config from './config';
 
 const app = express();
+
+const morganLogFormat = {
+  production: 'combined',
+  development: 'dev'
+}[config.environment()];
+
+
+app.use(morgan(morganLogFormat, {
+  stream:
+    config.environment() === 'production' ?
+      fs.createWriteStream('logs') :
+      process.stdout
+}));
+
 graphQLAPI.applyMiddleware({ app, path: '/graphql' });
+
 app.get("/", (_: Request, res: Response): Response => {
-  return res.status(200).send({ message: "Hello, Cleberson!" });
+  return res.status(200).send({ message: 'Hello, Cleberson!' });
 });
 
 export default app;
