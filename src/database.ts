8d24dca55fec367bb7bbe0d5aa7d211402b7de46
Cleberson F R Junior <cleberson.dev@gmail.com>
2021-02-06 23:14:59 -0300

    Defined knex config for development env

--- src/database.ts
+++ src/database.ts
@@ -1,12 +1,22 @@
 import knex from 'knex';
-import config from './config';
+import config, { EnvironmentTypes } from './config';
 
-export default knex({
-  client: 'pg',
-  connection: {
-    host: config.database.host,
-    user: config.database.username,
-    password: config.database.password,
-    database: config.database.name
+const dbConfigs: Record<EnvironmentTypes, knex.Config> = {
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './db.sqlite'
+    }
+  },
+  production: {
+    client: 'pg',
+    connection: {
+      host: config.database.host,
+      user: config.database.username,
+      password: config.database.password,
+      database: config.database.name
+    }
   }
-});
+}
+
+export default knex(dbConfigs[config.environment()]);
