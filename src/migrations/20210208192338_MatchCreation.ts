1be9737d927afe4e84ff05de1052a203372cb127
Cleberson F R Junior <cleberson.dev@gmail.com>
2021-02-08 19:52:22 -0300

    Created migration: MatchCreation

---
+++ src/migrations/20210208192338_MatchCreation.ts
@@ -0,0 +1,32 @@
+import * as Knex from "knex";
+
+const TABLE_NAME = 'match';
+
+export const up = async (knex: Knex): Promise<void> => (
+  knex.schema.createTable(TABLE_NAME, table => {
+    table.bigIncrements('match_id');
+    table.integer('round_number').notNullable();
+    table.integer('home_score').unsigned().defaultTo(0);
+    table.integer('away_score').unsigned().defaultTo(0);
+    table.string('match_place');
+    table.dateTime('match_time');
+
+    table.bigInteger('league')
+      .unsigned().notNullable()
+      .references('league_id').inTable('league');
+    
+    table.bigInteger('home_team')
+      .unsigned().notNullable()
+      .references('team_id').inTable('team');
+    
+    table.bigInteger('away_team')
+      .unsigned().notNullable()
+      .references('team_id').inTable('team');
+  })
+);
+
+
+export const down = async (knex: Knex): Promise<void> => (
+  knex.schema.dropTable(TABLE_NAME)
+);
+
