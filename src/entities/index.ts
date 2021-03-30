d6369dfb59f73266bdbdd46104086f8f07c25a83
Cleberson F R Junior <cleberson.dev@gmail.com>
2021-03-30 17:18:38 -0300

    refactor(domain): exported entities from index

---
+++ src/entities/index.ts
@@ -0,0 +1,13 @@
+import League from './League';
+import LeagueFixtures from './LeagueFixtures';
+import LeagueTeamStats from './LeagueTeamStats';
+import Match from './Match';
+import Team from './Team';
+import User from './User';
+
+export { 
+  League, LeagueFixtures, LeagueTeamStats,
+  Match,
+  Team,
+  User
+};
