925b57b033cdebea624dc09c942f5d9dcab8cfaa
cleberson-dev <cleberson.dev@gmail.com>
2021-04-03 14:46:42 -0300

    feat(user): implement repository

---
+++ src/main/database/UserRepository.ts
@@ -0,0 +1,78 @@
+import User from 'entities/User';
+import db from 'main/database';
+import { IUserRepository, SavedUserId } from 'common/repositories';
+
+export default class UserRepository implements IUserRepository {
+  private static _instance: IUserRepository = new UserRepository();
+
+  private constructor() {}
+  
+  static getInstance() {
+    return UserRepository._instance;
+  }
+  
+  async findOneByUsernameOrEmail(
+    usernameOrEmail: string,
+  ): Promise<User> {
+    const savedUser = await db('user_account')
+      .where({ username: usernameOrEmail })
+      .orWhere({ email: usernameOrEmail })
+      .first();
+
+    return User.create({
+      id: savedUser.user_id,
+      username: savedUser.username,
+      password: savedUser.password,
+      email: savedUser.email,
+      presentationName: savedUser.presentation_name,
+    });
+  }
+
+  async findAll(): Promise<User[]> {
+    const savedUsers = await db('user_account');
+    const users = savedUsers.map<User>((savedUser) =>
+      User.create({
+        id: savedUser.user_id,
+        username: savedUser.username,
+        password: savedUser.password,
+        email: savedUser.email,
+        presentationName: savedUser.presentation_name,
+      }),
+    );
+
+    return users;
+  }
+
+  async save(newUser: User): Promise<void> {
+    const table = db('user_account');
+
+    if (newUser.id) {
+      await table
+        .where({ user_id: newUser.id })
+        .first()
+        .update(newUser);
+    } else {
+      const [id] = await table.insert(newUser).returning('user_id');
+      newUser.id = id;
+    }
+  }
+
+  async findOneById(id: SavedUserId): Promise<User> {
+    const table = db('user_account');
+    const savedUser = await table
+      .where({
+        user_id: id,
+      })
+      .first();
+
+    if (!savedUser) throw Error('User not found');
+
+    return User.create({
+      id: savedUser.user_id,
+      username: savedUser.username,
+      password: savedUser.password,
+      email: savedUser.email,
+      presentationName: savedUser.presentation_name,
+    });
+  }
+}
