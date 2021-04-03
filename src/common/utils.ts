e347e30ad6c5184a43782ae1b94635a42ee2eb16
Cleberson F R Junior <cleberson.dev@gmail.com>
2021-04-02 23:29:16 -0300

    fix(test): added common util generateTeamsIds

--- src/common/utils.ts
+++ src/common/utils.ts
@@ -1,1 +1,11 @@
 export type PropType<TObj, TProp extends keyof TObj> = TObj[TProp];
+
+export function generateTeamsIds(amount: number) {
+  const teamsIds: number[] = []
+
+  for (let i = 1; i <= amount; i += 1) {
+    teamsIds.push(i);
+  }
+
+  return teamsIds;
+}
