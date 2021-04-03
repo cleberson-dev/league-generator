e33992c7d5fae79335b842d2af3f29b597492c82
cleberson-dev <cleberson.dev@gmail.com>
2021-04-03 14:54:02 -0300

    feat(team): graphql resolver

---
+++ src/main/graphql/resolvers/team.ts
@@ -0,0 +1,19 @@
+import { ITeamRepository, SavedTeamId } from "common/repositories"
+import { TeamRepository } from "main/database"
+import GetUserTeamsUseCase from "useCases/GetUserTeams"
+
+const teamRepository: ITeamRepository = TeamRepository.getInstance();
+
+export const query = {
+  myTeams: async (_: any, __: any, context: any) => {
+    const useCase = new GetUserTeamsUseCase(teamRepository);
+
+    const teams = await useCase.execute({ userId: context.userId });
+
+    return teams.map(team => ({
+      id: team.id as SavedTeamId,
+      name: team.name,
+      code: team.code
+    }));
+  }
+}
