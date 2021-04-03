8bdea4126ea4091e5ef7ca891cd6259c220e85a8
cleberson-dev <cleberson.dev@gmail.com>
2021-04-03 14:53:56 -0300

    feat(user): graphql resolver

---
+++ src/main/graphql/resolvers/user.ts
@@ -0,0 +1,47 @@
+import { IUserRepository } from "common/repositories";
+import Argon2Hasher from "main/Argon2Hasher";
+import { UserRepository } from "main/database";
+import JWTTokenGenerator from "main/JWTTokenGenerator";
+import LoginUserUseCase from "useCases/LoginUser";
+import RegisterUserUseCase from "useCases/RegisterUser";
+
+const userRepository: IUserRepository = UserRepository.getInstance();
+
+export const mutation = {
+  login: async (_: any, args: any) => {
+    const { usernameOrEmail, password } = args;
+
+    const useCase = new LoginUserUseCase(
+      userRepository,
+      new Argon2Hasher(),
+      new JWTTokenGenerator()
+    );
+
+    const result = await useCase.execute({ usernameOrEmail, password });
+      
+    return {
+      token: result.token,
+      user: {
+        username: result.user.username
+      }
+    };
+  },
+  register: async (_: any, args: any) => {
+    const { username, password, password2, email } = args;
+
+    const useCase = new RegisterUserUseCase(
+      userRepository,
+      new Argon2Hasher(),
+      new JWTTokenGenerator()
+    );
+
+    const result = await useCase.execute({ username, password, password2, email });
+
+    return {
+      token: result.token,
+      user: {
+        username: result.user.username
+      }
+    };
+  }
+}
