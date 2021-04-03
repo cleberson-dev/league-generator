daf7fe1ab6d7b1ae2ebd45dd2b2ca9cf59f9213a
cleberson-dev <cleberson.dev@gmail.com>
2021-04-03 14:50:30 -0300

    chore: export all route handlers from index

---
+++ src/main/routes/index.ts
@@ -0,0 +1,8 @@
+import userRouter from 'main/routes/user';
+import leagueRouter from 'main/routes/league';
+import matchRouter from 'main/routes/match';
+import teamRouter from 'main/routes/team';
+
+export {
+  userRouter, leagueRouter, matchRouter, teamRouter
+}
