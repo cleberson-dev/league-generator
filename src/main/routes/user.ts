cb7ae8d0894feceacbb7731f42e9407e84c92a0e
cleberson-dev <cleberson.dev@gmail.com>
2021-04-03 14:49:40 -0300

    feat(user): REST route handler

---
+++ src/main/routes/user.ts
@@ -0,0 +1,9 @@
+import { Router } from 'express';
+import UserController from 'main/controllers/user';
+
+const router = Router();
+
+router.post('/login', UserController.login);
+router.post('/register', UserController.register);
+
+export default router;
