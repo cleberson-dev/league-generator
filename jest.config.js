3c0cf0993bec9d4055f97260bfab11b815576799
cleberson-dev <cleberson.dev@gmail.com>
2021-04-03 22:36:02 -0300

    build: transform project-root imports to relative imports

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,10 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: [ "**/?(*.)+(spec|test).ts?(x)" ],
-  moduleDirectories: ['node_modules', 'src']
+  moduleDirectories: ['node_modules', 'src'],
+  globals: {
+    'ts-jest': {
+      compiler: 'ttypescript'
+    }
+  }
 };
